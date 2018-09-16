9b7c4ce79fe75767db096ce438c715c74f59a887
Stefano Pigozzi <stefano.pigozzi@gmail.com>
2018-09-16 17:00:23 +0200

    add sub2srs.ts

--- README.md
+++ README.md
@@ -10,3 +10,7 @@ analyzes the current subtitle with mecab, searches lemmas containing Kanji's
 in a dictionary, and displays them on the top left corner of the video
 
 <img src="https://0x0.st/s4Dg.png" width="600" title="jplookup">
+
+### sub2srs.ts
+
+exports the current subtitle to an Anki card complete with image and audio
---
+++ src/sub2srs.ts
@@ -0,0 +1,189 @@
+// adds the current subtitle to an anki deck
+// needs Anki Connect add-on - https://ankiweb.net/shared/info/2055492159
+
+const AUDIO_THRESHOLD = 0.25; // 250ms
+const IMAGE_WIDTH = 480;
+const IMAGE_DELAY_PERCENT = 0.08;
+const ANKI_DECK_NAME = 'sub2srs';
+const ANKI_NOTE_TYPE = 'Japanese sub2srs';
+const ANKI_TAG_NAME = 'sub2srs';
+const ANKI_MEDIA_COLLECTION =
+  '/Users/pigoz/Library/Application Support/Anki2/User 1/collection.media';
+const FFMPEG = 'ffmpeg';
+const CURL = 'curl';
+
+type path = string;
+
+function sts(ts: any): string {
+  const date = new Date(0);
+  date.setSeconds(ts);
+  const s = date.toISOString();
+  return s.slice(11, 23);
+}
+
+function mktemp(extension: string) {
+  const t = mp.utils.subprocess({
+    args: ['mktemp', '-t', 'sub2srs'],
+  });
+  return `${t.stdout.trim()}.${extension}`;
+}
+
+function cutaudio(path: path, aid: number, start: number, end: number): path {
+  const ss = sts(start - AUDIO_THRESHOLD);
+  const duration = sts(end - start + AUDIO_THRESHOLD);
+  const output = mktemp('mp3');
+
+  const command = [
+    FFMPEG,
+    '-y',
+    '-ss',
+    ss,
+    '-i',
+    path,
+    '-t',
+    duration,
+    '-map',
+    `0:a:${aid - 1}`,
+    output,
+  ];
+
+  const t = mp.utils.subprocess({ args: command });
+
+  if (t.status === 0) {
+    return output;
+  }
+
+  return '';
+}
+
+function screenshot(path: path, start: number, end: number): path {
+  const ss = sts(start + (end - start) * IMAGE_DELAY_PERCENT);
+  const output = mktemp('jpg');
+
+  const command = [
+    FFMPEG,
+    '-y',
+    '-ss',
+    ss,
+    '-i',
+    path,
+    '-vcodec',
+    'mjpeg',
+    '-vframes',
+    '1',
+    '-filter:v',
+    `scale=${IMAGE_WIDTH}:-1`,
+    output,
+  ];
+
+  const t = mp.utils.subprocess({ args: command });
+
+  if (t.status === 0) {
+    return output;
+  }
+
+  return '';
+}
+
+type ApiResult = {
+  result: undefined | unknown;
+  error: undefined | unknown;
+};
+
+function ankiapi(method: string, params: { [x: string]: unknown }): ApiResult {
+  const hostname = 'http://127.0.0.1:8765';
+  const data = JSON.stringify({ action: method, version: 6, params: params });
+
+  const command = [
+    CURL,
+    hostname,
+    '--silent',
+    // '--header',
+    // JSON.stringify('Content-Type: application/json'),
+    '--X',
+    'POST',
+    '--data',
+    data,
+  ];
+
+  mp.msg.warn(command.join(' '));
+  const t = mp.utils.subprocess({ args: command });
+  mp.msg.warn(JSON.stringify(t));
+  const reply = JSON.parse(t.stdout);
+  return reply;
+}
+
+function mv(src: string, dst: string) {
+  mp.utils.subprocess({ args: ['mv', src, dst] });
+}
+
+function addtoanki(
+  source: path,
+  start: number,
+  audio: path,
+  image: path,
+  line: string,
+): void {
+  const caudio = mp.utils.join_path(ANKI_MEDIA_COLLECTION, basename(audio));
+  const cimage = mp.utils.join_path(ANKI_MEDIA_COLLECTION, basename(image));
+
+  const fields = {
+    Source: source,
+    Time: sts(start),
+    Sound: `[sound:${basename(audio)}]`,
+    Image: `<img src="${basename(image)}" />`,
+    Line: line,
+  };
+
+  ankiapi('changeDeck', { cards: [], deck: ANKI_DECK_NAME });
+  const res = ankiapi('addNote', {
+    note: {
+      deckName: ANKI_DECK_NAME,
+      modelName: ANKI_NOTE_TYPE,
+      fields: fields,
+      tags: [ANKI_TAG_NAME],
+    },
+  });
+
+  // move files to the collection only if we successfully called the AnkiApi
+  if (res.result && !res.error) {
+    mv(audio, caudio);
+    mv(image, cimage);
+  }
+}
+
+function basename(path: string) {
+  const x = mp.utils.split_path(path);
+  return x[x.length - 1];
+}
+
+function sub2srs() {
+  const text = mp.get_property('sub-text');
+  const path = mp.get_property('path');
+  const aid = mp.get_property_number('aid') || 1;
+
+  if (!path) {
+    mp.msg.warn('no path available');
+    return;
+  }
+
+  if (!text) {
+    mp.msg.warn('no subtitle is focused');
+    return;
+  }
+
+  const start = mp.get_property_number('sub-start');
+  const end = mp.get_property_number('sub-end');
+
+  if (!start || !end) {
+    mp.msg.warn('cannot retrieve sub timings');
+    return;
+  }
+
+  const audio = cutaudio(path, aid, start, end);
+  const image = screenshot(path, start, end);
+
+  addtoanki(basename(path), start, audio, image, text.replace(/\n|\r/g, ''));
+}
+
+mp.add_key_binding('b', 'sub2anki', sub2srs);
